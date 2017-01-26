f749b9a82cbd801a93ce50a0de88045ce901594d
Thando Mini <tzone85@gmail.com>
2017-01-26 18:14:05 +0200

    added two more classes, also implementing the composition design pattern. The basics for now

--- src/Case.java
+++ src/Case.java
@@ -2,4 +2,35 @@
  * Created by Thando Mini on 1/26/2017.
  */
 public class Case {
+    private String model;
+    private String manufacture;
+    private String powerSupply;
+    private Dimensions dimensions;
+
+    public Case(String model, String manufacture, String powerSupply, Dimensions dimensions) {
+        this.model = model;
+        this.manufacture = manufacture;
+        this.powerSupply = powerSupply;
+        this.dimensions = dimensions;
+    }
+
+    public void presPowerButton(){
+        System.out.println("Power button pressed");
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getManufacture() {
+        return manufacture;
+    }
+
+    public String getPowerSupply() {
+        return powerSupply;
+    }
+
+    public Dimensions getDimensions() {
+        return dimensions;
+    }
 }
