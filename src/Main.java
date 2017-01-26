9a4223324e2317bdcea3fe86c4dbac15a983c036
Thando Mini <tzone85@gmail.com>
2017-01-26 18:52:34 +0200

    implemented composition design pattern more using the PC and the needed components to make it a pc

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,17 @@
 public class Main {
     public static void main(String [] args){
 
+        Dimensions dimensions = new Dimensions(20,20,5);
+        Case theCase = new Case("220B", "Lenovo", "350", dimensions);
+
+        Monitor theMonitor = new Monitor("30inch Beast", "Dell", 30, new Resolution(2540, 1440)); //creating a dimensions class on the fly (the dimensions class)
+
+        Motherboard theMotherboard = new Motherboard("Athlon", "Asus", 4, 6, "v3.55");
+
+        PC thePC = new PC(theCase, theMonitor, theMotherboard);
+
+        thePC.getMonitor().drawPixelAt(1500,1200,"blue");
+        thePC.getMotherboard().loadProgram("Fedora v.12.4");
+        thePC.getTheCase().presPowerButton();
     }
 }
---
+++ src/PC.java
@@ -0,0 +1,27 @@
+/**
+ * Created by Thando Mini on 1/26/2017.
+ */
+public class PC {
+
+    private Case theCase;
+    private Monitor monitor;
+    private Motherboard motherboard;
+
+    public PC(Case theCase, Monitor monitor, Motherboard motherboard) {
+        this.theCase = theCase;
+        this.monitor = monitor;
+        this.motherboard = motherboard;
+    }
+
+    public Case getTheCase() {
+        return theCase;
+    }
+
+    public Monitor getMonitor() {
+        return monitor;
+    }
+
+    public Motherboard getMotherboard() {
+        return motherboard;
+    }
+}
